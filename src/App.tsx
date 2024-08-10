dd894dc932bbbd3b3e022ee65391faf26c08b813
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2024-08-10 17:34:59 +0400

    add input value

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 function App() {
   return (
     <>
-    <main className="max-w-2xl mx-auto flex px-4">
-      <div className="py-8">
-      <h1 className="text-3xl font-bold uppercase mb-4">URL to Video<br/> with power of AI</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
-        voluptatum eligendi voluptates tempora praesentium, possimus impedit
-        nostrum qui temporibus numquam hic perspiciatis dicta nemo
-      </p>
-      <form>
-      <input type="url" placeholder="https://..." />
-      <button type="submit">Create video</button>
-      </form>
-      </div>
-      <div className="p-8">
-        <div className="bg-gray-200 w-[240px] h-[380px]">
-        a
+      <main className="max-w-4xl mx-auto flex px-4">
+        <div className="py-8">
+          <h1 className="text-3xl font-bold uppercase mb-8">
+            URL to Video
+            <br /> with power of AI
+          </h1>
+
+          <form className="border-2 rounded-full">
+            <input
+            className="bg-transparent text-white px-4 py-2 "
+            type="url" placeholder="https://..." />
+            <button type="submit">Create video</button>
+          </form>
         </div>
-      </div>
-    </main>
-    
+        <div className="p-8">
+          <div className="bg-gray-200 w-[240px] h-[380px]">a</div>
+        </div>
+      </main>
     </>
   );
 }
