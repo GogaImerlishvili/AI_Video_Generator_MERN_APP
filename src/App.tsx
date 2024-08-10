c73e669874a742f4ab225c1bcd8d136d2cfaf22b
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2024-08-10 14:13:25 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+function App() {
+  return (
+    <>
+    <main className="max-w-2xl mx-auto flex px-4">
+      <div className="p-8">
+      <h1 className="text-4xl font-bold">URL to Video<br/> with power of AI</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
+        voluptatum eligendi voluptates tempora praesentium, possimus impedit
+        nostrum qui temporibus numquam hic perspiciatis dicta nemo
+      </p>
+      <form>
+      <input type="url" placeholder="https://..." />
+      <button type="submit">Create video</button>
+      </form>
+      </div>
+      <div className="p-8">
+        <div className="bg-gray-200 w-[240px] h-[380px]">
+        a
+        </div>
+      </div>
+    </main>
+    
+    </>
+  );
+}
+
+export default App;
